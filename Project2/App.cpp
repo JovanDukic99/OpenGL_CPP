2d7f46897ab3ad0bb759a3ada8e690e09e7fbbe0
Dukson919 <34946205+Dukson919@users.noreply.github.com>
2021-01-25 23:03:51 +0100

    Update App.cpp

--- Project2/App.cpp
+++ Project2/App.cpp
@@ -1,8 +1,11 @@
 #pragma once
 #include "ServerSocket.h"
 #include "Config.h"
+#include <iostream>
 
 int main(int argc, char* argv[]) {
+
+	std::cout << argv[0] << std::endl;
 
 	ServerSocket* serverSocket = nullptr;
 
